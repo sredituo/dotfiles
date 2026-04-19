5eced43292caffbef93c881a183081005fa0419c
sredituo <b1508vms@gmail.com>
2026-04-19 18:01:43 +0900

    style: apply stylua

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -14,18 +14,16 @@ return {
 		vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#00ccff" })
 		-- コメント
 		vim.api.nvim_set_hl(0, "Comment", { fg = "#9ece6a", italic = true })
-        -- 未使用変数
-        vim.api.nvim_set_hl(0, "@lsp.mod.unused", { fg = "#6b7280", italic = true })
-        vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = "#6b7280", italic = true })
-        -- 行番号の色
+		-- 未使用変数
+		vim.api.nvim_set_hl(0, "@lsp.mod.unused", { fg = "#6b7280", italic = true })
+		vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = "#6b7280", italic = true })
+		-- 行番号の色
 		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#00ccff", bg = "none" })
 		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#00ccff", bg = "none" })
-        -- neotree
+		-- neotree
 		vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "none", fg = "#00ccff" })
-		-- Telescopeの透明化設定
-		-- TelescopeNormal = { fg = "#ebfafa", bg = "none" }
 	end,
 }
